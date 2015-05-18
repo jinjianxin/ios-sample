9ae098e186e7e9e60c270a48ff32c70d3e7463b0
jinjianxin <jinjianxinjin@gmail.com>
2015-05-18 16:45:02 +0800

    init project

---
+++ TestWangyi/TestWangyi/ViewController4.m
@@ -0,0 +1,37 @@
+//
+//  ViewController4.m
+//  TestWangyi
+//
+//  Created by jjx on 15/5/18.
+//  Copyright (c) 2015年 jjx. All rights reserved.
+//
+
+#import "ViewController4.h"
+
+@interface ViewController4 ()
+
+@end
+
+@implementation ViewController4
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
